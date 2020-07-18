d46a1c2ff8aa26de88aeed0f4381b0cd8c448547
cezmat-rep <36140797+cezmat-rep@users.noreply.github.com>
2020-07-18 23:18:07 +0200

    Add files via upload

---
+++ build/precache-manifest.a94ab5828f46c1c3bb499dd7b4a7f4b8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04137d976f981a2b7a38723422bd99fd",
+    "url": "/test-react/index.html"
+  },
+  {
+    "revision": "0ca78373709a04f8c4c3",
+    "url": "/test-react/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e63e4cee8347e2e81be9",
+    "url": "/test-react/static/js/2.6cf6baa2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/test-react/static/js/2.6cf6baa2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ca78373709a04f8c4c3",
+    "url": "/test-react/static/js/main.c2484775.chunk.js"
+  },
+  {
+    "revision": "b87ae238bc5eb9fb812c",
+    "url": "/test-react/static/js/runtime-main.00abedfe.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/test-react/static/media/logo.5d5d9eef.svg"
+  }
+]);
